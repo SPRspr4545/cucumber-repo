cea1ec372406af9976d378c100da8593ea7119df
SPRspr4545 <samuel.pelletier.06.37.57.34.89@gmail.com>
2025-04-23 16:24:52 +0200

    Add files via upload

---
+++ hooks/features/support/hooks.rb
@@ -0,0 +1,29 @@
+########## Before and After hooks
+Before do
+  puts "I'm called before the scenario runs!"
+end
+
+After do
+  puts "I'm called after the scenario runs!"
+end
+
+########## tagged hooks
+#Before('@test1002') do
+#  puts "I'm called before the scenario runs!"
+#end
+
+########## accessing the scenario object
+#Before do |scenario|
+#  puts "Beginning scenario: #{scenario.name}"
+#end
+
+#After do |scenario|
+#  puts "Something has gone wrong!" if scenario.failed?
+#end
+
+########## around hooks with a block.call
+#Around('@test1002') do |scenario, block|
+#  Timeout.timeout(0.0001) do
+#    block.call
+#  end
+#end
